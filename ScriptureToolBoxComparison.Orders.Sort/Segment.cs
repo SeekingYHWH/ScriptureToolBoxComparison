89291fcec336aa5ed039061ebbf61add24c80f5a
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 12:53:21 -0600

    added class internal sealed class Segment

---
+++ ScriptureToolBoxComparison.Orders.Sort/Segment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptureToolBoxComparison
+{
+	internal sealed class Segment
+	{
+		private readonly string value;
+		private readonly int offset;
+		private readonly int length;
+
+		public Segment(string value, int offset, int length)
+		{
+			this.value = value;
+			this.offset = offset;
+			this.length = length;
+		}
+
+		public string Value => value;
+		public int Offset => offset;
+		public int Length => length;
+	}
+}
