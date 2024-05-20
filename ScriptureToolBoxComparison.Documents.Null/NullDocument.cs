d57cda3cade3f1a7b2dd8f10453fa45764b688d2
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 12:14:05 -0600

    started library ScriptureToolBoxComparison.Documents.Null

--- ScriptureToolBoxComparison.Documents.Null/NullDocument.cs
+++ ScriptureToolBoxComparison.Documents.Null/NullDocument.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ScriptureToolBoxComparison
 {
-	internal sealed class NullDocument : IDocument
+	public sealed class NullDocument : IDocument
 	{
 		public NullDocument()
 		{
