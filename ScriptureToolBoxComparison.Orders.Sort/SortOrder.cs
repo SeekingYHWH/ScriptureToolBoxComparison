008081b6e8acbe596f28756bb22f58a07609f74f
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 12:47:32 -0600

    started class public sealed class SortOrder : IOrder

---
+++ ScriptureToolBoxComparison.Orders.Sort/SortOrder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptureToolBoxComparison
+{
+	public sealed class SortOrder : IOrder
+	{
+		private IDocument document;
+
+		public SortOrder()
+		{
+		}
+
+		public void SetDocument(IDocument document)
+		{
+			this.document = document;
+		}
+
+		public void ChapterStart(Chapter chapter)
+		{
+		}
+
+		public void ChapterFinish()
+		{
+		}
+
+		public void BookStart(Book book)
+		{
+		}
+
+		public void BookFinish()
+		{
+		}
+
+		public void WriteDelete(string value, int offset, int length)
+		{
+		}
+
+		public void WriteInsert(string value, int offset, int length)
+		{
+		}
+
+		public void WriteNormal(string value, int offset, int length)
+		{
+		}
+	}
+}
