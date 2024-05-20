6fde8d1888fe8a1621037010d2af7e118b36774c
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 13:17:35 -0600

    implementing

--- ScriptureToolBoxComparison.Orders.Sort/SortOrder.cs
+++ ScriptureToolBoxComparison.Orders.Sort/SortOrder.cs
@@ -7,6 +7,10 @@ namespace ScriptureToolBoxComparison
 	public sealed class SortOrder : IOrder
 	{
 		private IDocument document;
+		private readonly StringBuilder builder = new StringBuilder();
+		private Wrote wrote;
+		private readonly Queue<Segment> deletes = new Queue<Segment>();
+		private readonly Queue<Segment> inserts = new Queue<Segment>();
 
 		public SortOrder()
 		{
@@ -17,14 +21,6 @@ namespace ScriptureToolBoxComparison
 			this.document = document;
 		}
 
-		public void ChapterStart(Chapter chapter)
-		{
-		}
-
-		public void ChapterFinish()
-		{
-		}
-
 		public void BookStart(Book book)
 		{
 		}
@@ -33,15 +29,67 @@ namespace ScriptureToolBoxComparison
 		{
 		}
 
+		public void ChapterStart(Chapter chapter)
+		{
+			builder.Clear();
+			wrote = Wrote.None;
+			deletes.Clear();
+			inserts.Clear();
+		}
+
+		public void ChapterFinish()
+		{
+			throw new NotImplementedException();
+		}
+
 		public void WriteDelete(string value, int offset, int length)
 		{
+			WriteNormal();
+			deletes.Enqueue(new Segment(value, offset, length));
 		}
 
 		public void WriteInsert(string value, int offset, int length)
 		{
+			WriteNormal();
+			inserts.Enqueue(new Segment(value, offset, length));
 		}
 
 		public void WriteNormal(string value, int offset, int length)
+		{
+			//State
+			switch (wrote)
+			{
+			default:
+				wrote = Wrote.Normal;
+				break;
+
+			case Wrote.Delete:
+			case Wrote.Insert:
+				WriteOrdered();
+				builder.Append(' ');
+				wrote = Wrote.Normal;
+				break;
+
+			case Wrote.Normal:
+				builder.Append(' ');
+				break;
+			}
+			//Append
+			builder.Append(value, offset, length);
+		}
+
+		private void WriteNormal()
+		{
+			if (wrote != Wrote.Normal)
+			{
+				return;
+			}
+
+			document.WriteNormal(builder.ToString());
+
+		}
+
+		private void WriteOrdered()
 		{
 		}
 	}
