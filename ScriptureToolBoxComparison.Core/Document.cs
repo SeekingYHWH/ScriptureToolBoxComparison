4c105275755811f92ea711a6d88bacd8c20e0476
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 17:01:46 -0600

    added class public static class Document

---
+++ ScriptureToolBoxComparison.Core/Document.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptureToolBoxComparison
+{
+	public static class Document
+	{
+		public static bool NeedSpace(char letter)
+		{
+			switch (letter)
+			{
+			case '.':
+			case ':':
+			case ';':
+			case ',':
+				return false;
+
+			default:
+				return true;
+			}
+		}
+	}
+}
