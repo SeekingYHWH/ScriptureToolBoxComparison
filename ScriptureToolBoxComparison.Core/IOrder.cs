f5e94eb0994ab9d7e6d10d64135f1054e523244b
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 12:25:09 -0600

    added methods

--- ScriptureToolBoxComparison.Core/IOrder.cs
+++ ScriptureToolBoxComparison.Core/IOrder.cs
@@ -7,6 +7,10 @@ namespace ScriptureToolBoxComparison
 	public interface IOrder
 	{
 		void SetDocument(IDocument document);
+		void BookStart(Book book);
+		void BookFinish();
+		void ChapterStart(Chapter chapter);
+		void ChapterFinish();
 		void WriteDelete(string value, int offset, int length);
 		void WriteInsert(string value, int offset, int length);
 		void WriteNormal(string value, int offset, int length);
