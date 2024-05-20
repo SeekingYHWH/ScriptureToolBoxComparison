8cafd6b4156d648ab5323803edabd0273dc12d8d
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 12:23:11 -0600

    added method void SetDocument(IDocument document);

--- ScriptureToolBoxComparison.Core/IOrder.cs
+++ ScriptureToolBoxComparison.Core/IOrder.cs
@@ -6,6 +6,7 @@ namespace ScriptureToolBoxComparison
 {
 	public interface IOrder
 	{
+		void SetDocument(IDocument document);
 		void WriteDelete(string value, int offset, int length);
 		void WriteInsert(string value, int offset, int length);
 		void WriteNormal(string value, int offset, int length);
