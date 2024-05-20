12b157ecd89585d360b4cc6ac7134948ab8b8edc
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-20 11:58:57 -0600

    readonly

--- ScriptureToolBoxComparison.Core/Chapter.cs
+++ ScriptureToolBoxComparison.Core/Chapter.cs
@@ -6,8 +6,8 @@ namespace ScriptureToolBoxComparison
 {
 	public sealed class Chapter
 	{
-		private int name;
-		private string source;
+		private readonly int name;
+		private readonly string source;
 
 		public Chapter(int name, string source)
 		{
