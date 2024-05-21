23f02a4563145bfbd59803b38a40c50170476af6
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-21 15:13:14 -0600

    added Writings.config

--- ScriptureToolBoxComparison/Program.cs
+++ ScriptureToolBoxComparison/Program.cs
@@ -36,7 +36,7 @@ namespace ScriptureToolBoxComparison
 		#region Methods
 		private static List<string> ParseCommandLine(string[] args)
 		{
-			var name = "Prophets";
+			var name = "Writings";
 			document = new MultipleDocument(
 				new HTMLDocument(new XmlDocument().DocumentElement, name + ".html"),
 				new TextLog(new XmlDocument().DocumentElement, name + ".log"));
