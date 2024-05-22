3dcbfe501eb6d9a32c2cb1070f00ebe1be7959cf
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-22 13:04:11 -0600

    stubbed class public class GemBoxDocument : IDocument

---
+++ ScriptureToolBoxComparison.Documents.GemBox/GemBoxDocument.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace ScriptureToolBoxComparison
+{
+	public class GemBoxDocument : IDocument
+	{
+		public GemBoxDocument()
+		{
+		}
+
+		~GemBoxDocument()
+		{
+			Dispose(disposing: false);
+		}
+
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+		}
+
+		private void Dispose(bool disposing)
+		{
+		}
+
+		public void BookStart(Book book)
+		{
+		}
+
+		public void BookFinish()
+		{
+		}
+
+		public void ChapterStart(Chapter chapter)
+		{
+		}
+
+		public void ChapterFinish()
+		{
+		}
+
+		public void WriteDelete(string text)
+		{
+		}
+
+		public void WriteInsert(string text)
+		{
+		}
+
+		public void WriteNormal(string text)
+		{
+		}
+	}
+}
