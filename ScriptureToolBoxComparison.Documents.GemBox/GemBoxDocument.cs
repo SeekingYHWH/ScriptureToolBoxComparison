1e774cc6d8f8d1e9ed7ba5d93b5058779e070d11
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-22 13:36:21 -0600

    implemented public class GemBoxDocument : IDocument

--- ScriptureToolBoxComparison.Documents.GemBox/GemBoxDocument.cs
+++ ScriptureToolBoxComparison.Documents.GemBox/GemBoxDocument.cs
@@ -5,6 +5,8 @@ using System.Xml;
 
 using GemBox.Document;
 
+using static System.Net.Mime.MediaTypeNames;
+
 namespace ScriptureToolBoxComparison
 {
 	public class GemBoxDocument : IDocument
@@ -14,8 +16,35 @@ namespace ScriptureToolBoxComparison
 			ComponentInfo.SetLicense("FREE-LIMITED-KEY");
 		}
 
+		private readonly string path;
+		private readonly DocumentModel document;
+		private readonly CharacterFormat book;
+		private readonly CharacterFormat chapter;
+		private readonly CharacterFormat delete;
+		private readonly CharacterFormat insert;
+		private readonly CharacterFormat normal;
+		private readonly Section section;
+		private Paragraph paragraph;
+
 		public GemBoxDocument(XmlNode config, string path)
 		{
+			this.path = path;
+
+			this.document = new DocumentModel();
+			var heading1 = (CharacterStyle)Style.CreateStyle(StyleTemplateType.Heading1, document);
+			//heading1.CharacterFormat = new CharacterFormat() { };
+			document.Styles.Add(heading1);
+			var heading3 = (CharacterStyle)Style.CreateStyle(StyleTemplateType.Heading3, document);
+			//heading3.CharacterFormat = new CharacterFormat() { };
+			document.Styles.Add(heading3);
+			this.book = new CharacterFormat() { Style = heading1, };
+			this.chapter = new CharacterFormat() { Style = heading3, };
+			this.delete = new CharacterFormat() { Bold = false, Strikethrough = true, };
+			this.insert = new CharacterFormat() { Bold = true, Strikethrough = false, };
+			this.normal = new CharacterFormat() { Bold = false, Strikethrough = false, };
+			this.section = new Section(document);
+			section.PageSetup.PaperType = PaperType.Letter;
+			document.Sections.Add(section);
 		}
 
 		~GemBoxDocument()
@@ -30,34 +59,59 @@ namespace ScriptureToolBoxComparison
 
 		private void Dispose(bool disposing)
 		{
+			document.Save(path);
 		}
 
-		public void BookStart(Book book)
+		public void BookStart(Book value)
 		{
+			paragraph = new Paragraph(document);
+			section.Blocks.Add(paragraph);
+			var run = new Run(document);
+			run.CharacterFormat = delete.Clone();
+			run.Text = value.Name;
+			paragraph.Inlines.Add(run);
+			paragraph = new Paragraph(document);
+			section.Blocks.Add(paragraph);
 		}
 
 		public void BookFinish()
 		{
 		}
 
-		public void ChapterStart(Chapter chapter)
+		public void ChapterStart(Chapter value)
 		{
+			var run = new Run(document);
+			run.CharacterFormat = chapter.Clone();
+			run.Text = value.Name;
+			paragraph.Inlines.Add(run);
 		}
 
 		public void ChapterFinish()
 		{
 		}
 
-		public void WriteDelete(string text)
+		public void WriteDelete(string value)
 		{
+			var run = new Run(document);
+			run.CharacterFormat = delete.Clone();
+			run.Text = value;
+			paragraph.Inlines.Add(run);
 		}
 
-		public void WriteInsert(string text)
+		public void WriteInsert(string value)
 		{
+			var run = new Run(document);
+			run.CharacterFormat = insert.Clone();
+			run.Text = value;
+			paragraph.Inlines.Add(run);
 		}
 
-		public void WriteNormal(string text)
+		public void WriteNormal(string value)
 		{
+			var run = new Run(document);
+			run.CharacterFormat = normal.Clone();
+			run.Text = value;
+			paragraph.Inlines.Add(run);
 		}
 	}
 }
