85ba55328d683a09c44d5126f0077800ad26eafc
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-22 15:28:39 -0600

    stubbed class public sealed class FreeSpireDocument : IDocument

---
+++ ScriptureToolBoxComparison.Documents.FreeSpire/FreeSpireDocument.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptureToolBoxComparison
+{
+	public sealed class FreeSpireDocument : IDocument
+	{
+		public FreeSpireDocument()
+		{
+		}
+
+		~FreeSpireDocument()
+		{
+			Dispose(disposing: false);
+		}
+
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+		}
+
+		private void Dispose(bool disposing)
+		{
+		}
+
+		public void BookStart(Book book)
+		{
+		}
+
+		public void BookFinish()
+		{
+		}
+
+		public void ChapterStart(Chapter chapter)
+		{
+		}
+
+		public void ChapterFinish()
+		{
+		}
+
+		public void WriteDelete(string text)
+		{
+		}
+
+		public void WriteInsert(string text)
+		{
+		}
+
+		public void WriteNormal(string text)
+		{
+		}
+	}
+}
