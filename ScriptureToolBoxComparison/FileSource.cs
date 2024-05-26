d2d7840aab45017330095bfd762b87d292ca4bad
SeekingYHWH <SJR3t2+SeekingYHWH@RetzNest.com>
2024-05-26 17:11:54 -0600

    adde class public Stream GetStream(Chapter value)

---
+++ ScriptureToolBoxComparison/FileSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ScriptureToolBoxComparison
+{
+	internal sealed class FileSource : ISource
+	{
+		private readonly string path;
+
+		public FileSource(string path)
+		{
+			this.path = path;
+		}
+
+		public Stream GetStream(Chapter value)
+		{
+			var streamPath = Path.Combine(path, value.Source);
+			var stream = new FileStream(streamPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+			return stream;
+		}
+	}
+}
